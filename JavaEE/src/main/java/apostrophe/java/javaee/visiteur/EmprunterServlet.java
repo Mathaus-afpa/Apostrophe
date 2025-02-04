6a24b11160e36130cf535d821fd9dabb039604c9
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2025-02-04 16:41:42 +0100

    Ajout de la redirection Emprunt pour les visiteurs.

---
+++ JavaEE/src/main/java/apostrophe/java/javaee/visiteur/EmprunterServlet.java
@@ -0,0 +1,20 @@
+package apostrophe.java.javaee.visiteur;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+@WebServlet("/Emprunter")
+public class EmprunterServlet  extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
+		// Récupérer la session existante (false pour ne pas créer une nouvelle session si elle n'existe pas)
+		HttpSession session = request.getSession(false);
+		// Vérifier si la session existe et si l'utilisateur est bien authentifié
+		if (session == null || session.getAttribute("username") == null) {
+			response.sendRedirect("/Connexion");  // Rediriger vers la page de login si pas authentifié
+			return;
+		}
+	}
+}
